c71b591e7ca3bad4add2d0fbd5e14aee84082494
Ulrich Scheller <Ulrich Scheller@localhost>
2011-03-02 17:26:16 +0100

    added comments with example uses of the functions

--- src/classify.py
+++ src/classify.py
@@ -2,6 +2,20 @@ import os
 j = os.path.join
 
 def train(text):
+	"""
+	Train a dictionary with the given text. Returns a dictionary of dictionaries, 
+	that describes the probabilities of all word-folling-ocurrences in the text.
+	
+	For example, the string "a test" gives this result:
+	>>> train("a test")
+	{'': {'a': 1}, 'a': {'test': 1}}
+	Meaning that the empty string '' has been followed once by 'a' and 'a' has been 
+	followed by 'test' as well.
+	
+	A longer example leads to a more complex dictionary:
+	>>> train("this is a test oh what a test")
+	{'': {'this': 1}, 'a': {'test': 2}, 'what': {'a': 1}, 'oh': {'what': 1}, 'this': {'is': 1}, 'is': {'a': 1}, 'test': {'oh': 1}}
+	"""
 	c = {}
 	lastword = ""
 	for word in text.split():
@@ -18,6 +32,17 @@ def train(text):
 	return c
 
 def probability_of(dict, lastword, word):
+	"""
+	Helper function for calculating the probability of word following lastword
+	in the category given by dict.
+	
+	>>> category = train("this is a test")
+	>>> probability_of(category, "a", "test")
+	1.0
+	
+	>>> probability_of(category, "any", "words")
+	0
+	"""
 	word = word.lower()
 	if dict.has_key(lastword):
 		inner = dict[lastword]
@@ -27,6 +52,18 @@ def probability_of(dict, lastword, word):
 	return 0
 
 def classify(text, dict):
+	"""
+	Returns the probability that a text is from the given category. For every pair of
+	words the probability_of value is calculated, summarized and divided by the amount
+	of words in the text.
+	
+	>>> category = train("this is a test")
+	>>> classify("a test with some words", category)
+	0.2
+	
+	>>> classify("just writing test or a doesn't improve the ranking", category)
+	0.0
+	"""
 	lastword = ""
 	probabilities = 0
 	for word in text.split():
@@ -35,7 +72,9 @@ def classify(text, dict):
 	return probabilities / (len(text.split()) * 1.0)
 
 if __name__ == "__main__":
-	best = 0
+	"""
+	Calculate the category, that the text in ../test matches best.
+	"""
 	ranking = []
 	os.chdir("..")
 	for file in os.listdir("categories"):
